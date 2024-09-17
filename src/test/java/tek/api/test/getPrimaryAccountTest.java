af6f733809c9ecf30c183ef91045c79898126157
zianzahid <ziamustafa2000@hotmail.com>
2024-09-17 16:55:35 -0700

    new commit API

--- src/test/java/tek/api/test/getPrimaryAccountTest.java
+++ src/test/java/tek/api/test/getPrimaryAccountTest.java
@@ -3,13 +3,14 @@ package tek.api.test;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 import tek.tdd.api.models.EndPoints;
 import tek.tdd.base.ApiTestsBase;
 
 import static io.restassured.RestAssured.basePath;
 import static io.restassured.RestAssured.requestSpecification;
 import static jdk.internal.org.jline.utils.Colors.s;
-
+@Test
 public class getPrimaryAccountTest extends ApiTestsBase {
     public void getAccountAndValidate() {
         RequestSpecification.querryParam("primaryPersonId",10035);
